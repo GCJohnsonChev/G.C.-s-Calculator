a0654f078eda65b0359ea2fc517ac78133c6fa00
Gary Christopher Johnson <47867437+GCJohnsonChev@users.noreply.github.com>
2022-09-14 15:18:52 -0700

    Update G.C.'s - Calculator.c

--- C++/G.C.'s - Calculator.c
+++ C++/G.C.'s - Calculator.c
@@ -1,3 +1,30 @@
+/*
+ ===========================================================================================
+ + G.C.'s - Calculator
+ + ---
+ + - Author : Gary Christopher Johnson
+ + - E-Mail : TinkeSoftware@Protonmail.com
+ + - Company: Tinke Software
+ + - Notes  : View this file in a non-formatting text editor for correct indentation display
+ + ---
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ +
+ + ---
+ + - File Location:
+ + - File Version:
+ + ---
+*/
+
+
 int main () {
 
 var a = 1;
